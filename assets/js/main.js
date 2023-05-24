68fd61121510cfb2f6565dc4a9c7bc4fe0cf9de4
Scott Gingras <greanbaby@gmail.com>
2023-05-23 23:38:21 -0600

    added code blocks

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,18 @@ const menuToggle = document.querySelector('#menu-toggle');
 menuToggle.addEventListener('click', function () {
     menuToggle.classList.toggle('open');
 });
+
+var coll = document.getElementsByClassName("collapsible-btn");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.classList.contains("show")) {
+      content.classList.remove("show");
+    } else {
+      content.classList.add("show");
+    }
+  });
+}
